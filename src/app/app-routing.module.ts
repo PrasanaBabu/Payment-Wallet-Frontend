003369ab86cbb66cdb3e3a8a996f6b82240e26c7
PrasanaBabu <prasanajamuna@gmail.com>
2022-12-06 10:05:17 +0530

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddFundComponent } from './components/add-fund/add-fund.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { UnregisterComponent } from './components/unregister/unregister.component';
+import { WithdrawComponent } from './components/withdraw/withdraw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'wallet/home', component:HomeComponent},
+  {path: 'wallet/login', component:LoginComponent},
+  {path: 'wallet/register', component:RegisterComponent},
+  {path: 'wallet/addfund', component:AddFundComponent},
+  {path: 'wallet/withdraw', component: WithdrawComponent},
+  {path: 'wallet/transfer', component: TransferComponent},
+  {path: 'wallet/unregister', component:UnregisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddFundComponent } from './components/add-fund/add-fund.component';
+import { WithdrawComponent } from './components/withdraw/withdraw.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { UnregisterComponent } from './components/unregister/unregister.component';
+import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+
+
+import { PasswordModule } from "primeng/password";
+import { DividerModule } from "primeng/divider";
+import { MatchPasswordDirective } from './directives/match-password.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddFundComponent,
+    WithdrawComponent,
+    TransferComponent,
+    UnregisterComponent,
+    MatchPasswordDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ButtonModule,
+    PasswordModule,
+    DividerModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
