a21aaea95ea3130d029019b09cac79af3e8f3ca7
PrasanaBabu <prasanajamuna@gmail.com>
2022-12-07 09:12:58 +0530

    Guarding and Add Fund Component Done

--- src/app/components/add-fund/add-fund.component.ts
+++ src/app/components/add-fund/add-fund.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ConfirmationService, Message, PrimeNGConfig } from 'primeng/api';
+import { Transaction } from 'src/app/dto/transaction';
+import { WalletService } from 'src/app/services/wallet.service';
 
 @Component({
   selector: 'app-add-fund',
@@ -6,5 +9,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-fund.component.css']
 })
 export class AddFundComponent {
+
+  msgs: Message[] = [];
+  errMsg: "";
+  userId: number;
+  amountToAdd = "";
+  transaction: Transaction = new Transaction();
+
+  success: String;
+
+
+  position: string;
+
+  constructor(private confirmationService: ConfirmationService,
+    private primengConfig: PrimeNGConfig,
+    private walletService: WalletService) {
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
+  confirm() {
+    this.confirmationService.confirm({
+      message: 'Are you sure  you want to add ' + this.amountToAdd + ' to ID: ' + this.userId,
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        if (this.userId == undefined || this.amountToAdd == "") {
+          this.msgs = [{ severity: 'error', summary: 'Rejected', detail: 'Please Enter Id and amount' }];
+
+        }
+        else {
+          this.msgs = [
+            {
+              severity: 'info',
+              summary: 'Confirmed',
+              detail: 'Request Successfully sent to server please wait for confirmation'
+            }
+          ];
+          this.addFund();
+        }
+      },
+      reject: () => {
+        this.msgs = [{ severity: 'info', summary: 'Rejected', detail: 'You have rejected' }];
+      }
+    });
+  }
+
+  addFund() {
+
+    console.log("Accept method clicked with amount = " + this.amountToAdd + " id = " + this.userId);
+    this.transaction.amount = parseInt(this.amountToAdd);
+    this.transaction.customerId = this.userId;
+
+    this.walletService
+      .credit(this.transaction)
+      .subscribe(
+        {
+          next: data => { data = this.success },
+
+          error: err => { this.errMsg = err }
+
+
+        }
+      );
+
+    console.log("succ = " + this.success);
+  }
 
 }
